0edc7ff4f6ebbae0abd9d4625d7cac81acb759ab
Vikash Kumar <vikashloharjsr@gmail.com>
2025-01-01 22:19:07 +0530

    basic types

---
+++ docs/typescript/01_basics/types.ts
@@ -0,0 +1,7 @@
+// There are 3 primitive types in TypeScript: number, string, and boolean
+
+let myNumber: number = 3;
+let myString: string = "Docs.dev";
+let myBoolean: boolean = true;
+
+myNumber = function(){}
